fd414c968a489462f2a6f4c021cbeca02f261bc1
Aubin <eyengadjou@gmail.com>
2024-05-05 08:36:22 +0100

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {
+    // Filtrer ou ignorer les avertissements spécifiques
+    if (config.stats) {
+      config.stats.warningsFilter = (warning) => {
+        return (
+          warning.indexOf(
+            "Extra attributes from the server: cz-shortcut-listen"
+          ) > -1
+        );
+      };
+    }
+
+    return config;
+  },
+};
 
 export default nextConfig;
