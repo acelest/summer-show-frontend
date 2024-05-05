ca23cb018ada8bf22f4acc78fc4676a16aece728
Aubin <eyengadjou@gmail.com>
2024-05-05 08:47:20 +0100

    fix bug app

--- src/app/page.js
+++ src/app/page.js
@@ -1,2 +1,26 @@
-import "./globals.css";
-export default function Home() {}
+// pages/index.js
+
+import Banner from "@/components/Banner";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+// import Layout from "@/components/Layout";
+export default function Home() {
+  return (
+    <div className="bg-black ">
+      <Header />
+      <div>
+        <h1>Welcome to My Next.js App!</h1>
+        <p>This is the homepage of my Next.js application.</p>
+        <Banner />
+        <Banner />
+        <Banner />
+        <Banner />
+        <Banner />
+        <Banner />
+        <Banner />
+        <Banner />
+      </div>
+      <Footer />
+    </div>
+  );
+}
