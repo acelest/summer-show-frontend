cff0f4dc61fac97a8daca7c3aa59a6d2b6d3a017
Aubin <eyengadjou@gmail.com>
2024-05-07 05:02:56 +0100

    Category section

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 // pages/index.js
 
 import Banner from "@/components/Banner";
+import Category from "@/components/CategorySection";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "./globals.css";
@@ -14,6 +15,7 @@ export default function Home() {
         <h1 className=" text-blue-50">Welcome to My Next.js App!</h1>
         <p>This is the homepage of my Next.js application.</p>
         <Banner />
+        <Category />
       </div>
       <Footer />
     </div>
---
+++ src/components/CategorySection.jsx
@@ -0,0 +1,12 @@
+"use client"; // Add this line at the top of the file
+
+export default function Category() {
+  return (
+    <div className=" bg-white h-20">
+      <p className=" text-center">Categorie...</p>
+      <p></p>
+      <p></p>
+      <p></p>
+    </div>
+  );
+}
