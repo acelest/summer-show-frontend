6fae768ba5c83cade9375c9a283440073715c1ac
Aubin <eyengadjou@gmail.com>
2024-05-08 16:37:23 +0100

    components

--- src/components/CategorySection.jsx
+++ src/components/CategorySection.jsx
@@ -1,12 +1,175 @@
-"use client"; // Add this line at the top of the file
+"use client";
+import "./category.css";
+// import Mode from "..//assets/mode.svg"
+const DanceIcon = () => (
+  <svg
+    version="1.0"
+    xmlns="http://www.w3.org/2000/svg"
+    width="45.00000pt"
+    height="45.00000pt"
+    viewBox="0 0 1105.000000 1280.000000"
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <metadata>
+      Created by potrace 1.15, written by Peter Selinger 2001-2017
+    </metadata>
+    <g
+      transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)"
+      fill="#000000"
+      stroke="none"
+    >
+      <path
+        d="M5873 12786 c-118 -27 -207 -83 -233 -146 -10 -24 -19 -30 -42 -30
+-41 0 -185 -37 -259 -65 -255 -100 -462 -311 -570 -582 -71 -175 -104 -333
+-120 -568 -6 -82 -13 -152 -15 -154 -4 -4 -354 64 -769 149 -251 52 -323 63
+-545 80 -178 13 -495 13 -594 0 -205 -29 -309 -94 -309 -192 0 -135 174 -275
+533 -428 319 -137 855 -295 1322 -390 104 -21 192 -41 195 -43 4 -4 -25 -117
+-32 -125 -2 -2 -32 5 -67 16 -250 77 -446 -5 -556 -232 -61 -124 -82 -211 -82
+-331 0 -138 36 -257 113 -378 32 -50 35 -59 26 -89 -17 -60 -23 -211 -9 -267
+10 -42 25 -66 74 -118 74 -80 155 -123 231 -123 44 0 230 42 243 54 2 3 27 -4
+56 -15 48 -20 81 -21 133 -4 19 6 23 2 29 -21 26 -105 114 -280 200 -397 24
+-32 67 -104 96 -161 l52 -104 -44 -41 c-25 -22 -48 -40 -51 -41 -6 0 -186 149
+-469 390 -324 276 -535 417 -875 585 -126 62 -306 144 -400 180 -216 86 -636
+225 -920 305 l-220 63 -52 -59 c-29 -32 -55 -56 -57 -53 -3 3 -24 34 -46 68
+-137 212 -343 413 -553 540 -410 247 -710 266 -831 52 -39 -71 -43 -166 -8
+-222 27 -44 112 -125 171 -165 l45 -29 -125 -6 c-199 -10 -328 -53 -424 -141
+-162 -147 -148 -385 27 -472 49 -24 61 -26 197 -26 125 0 164 -4 269 -29 68
+-16 125 -31 128 -34 3 -3 -16 -9 -43 -13 -83 -12 -195 -53 -268 -99 -41 -25
+-112 -87 -171 -147 -91 -93 -103 -110 -125 -173 -30 -89 -32 -196 -6 -240 22
+-37 71 -72 118 -85 19 -6 81 -10 139 -10 103 0 106 1 165 36 91 55 396 206
+403 199 7 -7 -25 -103 -74 -220 -21 -49 -39 -113 -42 -141 -13 -156 110 -274
+296 -282 95 -5 153 8 215 48 50 32 79 84 101 185 17 75 67 184 97 211 17 15
+19 15 19 1 0 -14 37 -15 353 -10 550 9 896 -22 1272 -112 587 -141 992 -421
+1197 -830 34 -67 35 -72 26 -130 -11 -69 -4 -149 18 -218 15 -47 15 -48 -14
+-110 -41 -87 -110 -170 -277 -329 -245 -234 -348 -350 -493 -556 -152 -215
+-334 -546 -477 -867 -65 -144 -222 -533 -261 -643 -17 -50 -27 -60 -169 -171
+-695 -545 -1072 -947 -1350 -1441 -26 -47 -49 -86 -50 -88 -1 -2 -52 -16 -112
+-32 -484 -127 -920 -447 -1102 -810 -76 -150 -96 -259 -95 -510 1 -145 6 -198
+28 -310 39 -201 31 -187 130 -261 283 -215 744 -325 1223 -291 681 49 1297
+313 1794 769 150 136 152 140 144 220 -19 193 -65 357 -149 526 l-63 127 28
+70 c172 426 746 1057 1145 1258 l61 31 38 -26 c66 -45 203 -104 303 -130 79
+-21 127 -26 262 -30 l165 -5 52 -40 c89 -68 257 -250 326 -354 74 -111 163
+-291 201 -404 70 -212 96 -362 103 -608 l6 -193 -80 -74 c-86 -80 -238 -269
+-290 -363 -103 -181 -77 -340 81 -498 211 -211 817 -516 1306 -659 379 -110
+664 -150 1075 -150 l280 0 94 33 c130 45 205 87 277 154 114 107 162 244 151
+433 -29 491 -323 962 -781 1249 l-87 55 -4 113 c-9 279 -91 610 -226 911 -104
+234 -255 478 -448 726 l-110 140 66 68 c36 37 90 98 120 135 l55 68 -20 107
+c-40 208 -64 407 -88 728 -21 272 -20 1048 1 1390 8 138 18 298 22 357 3 59 9
+111 12 117 5 7 48 7 136 0 595 -47 1188 142 1685 539 138 110 401 376 524 532
+116 146 288 396 381 554 l58 98 -27 67 c-36 86 -106 208 -172 297 l-52 71 49
+37 c59 44 121 120 160 197 l28 57 78 -32 c200 -83 452 -118 572 -79 63 20 128
+74 152 127 22 48 25 139 7 199 -18 61 -96 139 -183 184 -56 29 -75 43 -75 59
+0 36 23 50 115 68 218 43 306 90 356 191 28 57 31 72 26 121 -3 32 -16 73 -31
+98 -35 60 -121 135 -204 179 -70 37 -70 37 -195 37 -135 0 -243 -16 -370 -56
+-43 -14 -81 -24 -83 -21 -2 2 10 35 27 73 42 91 65 159 95 274 35 137 40 195
+20 264 -53 180 -300 268 -486 173 -115 -59 -235 -308 -305 -635 -26 -123 -63
+-215 -92 -230 -32 -17 -79 12 -187 117 -171 166 -295 229 -434 219 -108 -8
+-182 -59 -233 -158 -42 -85 -36 -268 13 -364 11 -22 44 -65 73 -96 52 -55 52
+-55 73 -170 38 -207 44 -284 40 -504 -6 -241 -23 -350 -89 -549 -93 -284 -238
+-519 -448 -727 -208 -205 -447 -344 -706 -410 -117 -30 -316 -38 -424 -16
+-142 29 -182 48 -222 108 -40 60 -112 113 -175 129 -23 6 -50 19 -59 29 -15
+17 -14 23 12 86 l29 69 89 48 c251 136 454 345 580 596 32 64 39 72 58 66 12
+-3 60 -13 107 -22 192 -35 378 33 540 196 120 120 203 267 241 428 18 75 20
+109 16 223 -6 153 -28 252 -81 365 -82 173 -227 256 -427 241 -71 -4 -89 -3
+-99 9 -15 18 -19 15 53 51 122 60 108 130 -38 194 l-73 32 -16 65 c-9 39 -12
+71 -7 77 13 16 -19 482 -45 661 -103 699 -446 1111 -1079 1295 -57 17 -110 30
+-118 30 -12 0 -14 9 -10 38 9 70 -76 152 -195 188 -77 22 -230 27 -309 10z"
+      />
+    </g>
+  </svg>
+);
+
+// const RapIcon = () => {};
 
 export default function Category() {
   return (
-    <div className=" bg-white h-20">
-      <p className=" text-center">Categorie...</p>
-      <p></p>
-      <p></p>
-      <p></p>
+    <div className="h-auto bg-red-300 mt-4">
+      <div className="bg-gray-100 pb-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 main px-5 gap-4">
+        <a href="#" className="block">
+          <div
+            className="bg-gradient-to-b flex flex-col text-white text-2sm rounded-2xl text-center gap-2 font-bold background-image"
+            style={{
+              backgroundImage:
+                'url("https://cdn.sanity.io/images/c09exlh6/production/7f2c68e34b5b2b3dc37f91ef2ecd4c6a9997bdc1-1280x946.png")',
+              backgroundSize: "cover",
+            }}
+          />
+          <div className="c8_sh py-6">
+            <div className="flex flex-row justify-center">
+              <DanceIcon />
+            </div>
+            <span className="text-center flex justify-center jersey-25-charted-regular font-bold">
+              Danse
+            </span>
+          </div>
+        </a>
+        <a href="#" className="block">
+          <div
+            className="bg-gradient-to-b flex flex-col text-white text-2sm rounded-2xl text-center gap-2 font-bold background-image"
+            style={{
+              backgroundImage:
+                'url("https://cdn.sanity.io/images/c09exlh6/production/7f2c68e34b5b2b3dc37f91ef2ecd4c6a9997bdc1-1280x946.png")',
+              backgroundSize: "cover",
+            }}
+          />
+          <div className="c8_sh py-6">
+            <div className="flex flex-row justify-center">
+              <DanceIcon />
+            </div>
+          </div>
+        </a>
+        <a href="#" className="block">
+          <div
+            className="bg-gradient-to-b flex flex-col text-white text-2sm rounded-2xl text-center gap-2 font-bold background-image"
+            style={{
+              backgroundImage:
+                'url("https://cdn.sanity.io/images/c09exlh6/production/7f2c68e34b5b2b3dc37f91ef2ecd4c6a9997bdc1-1280x946.png")',
+              backgroundSize: "cover",
+            }}
+          />
+          <div className="c8_sh py-6">
+            <div className="flex flex-row justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="50px"
+                height="50px"
+              >
+                <path
+                  fill="currentColor"
+                  d="M15 2h1c.552 0 1 .448 1 1v2c0 .552-.448 1-1 1h-1c-.552 0-1-.448-1-1V3c0-.552.448-1 1-1zm-1 1v2H6V3h8zM3 6h18c.552 0 1 .448 1 1v14c0 .552-.448 1-1 1H3c-.552 0-1-.448-1-1V7c0-.552.448-1 1-1zm0 16h18V8H3v14zM7 10h10v2H7v-2zm0 4h10v2H7v-2z"
+                ></path>
+              </svg>
+            </div>
+          </div>
+        </a>
+        <a href="#" className="block">
+          <div
+            className="bg-gradient-to-b flex flex-col text-white text-2sm rounded-2xl text-center gap-2 font-bold background-image"
+            style={{
+              backgroundImage:
+                'url("https://cdn.sanity.io/images/c09exlh6/production/7f2c68e34b5b2b3dc37f91ef2ecd4c6a9997bdc1-1280x946.png")',
+              backgroundSize: "cover",
+            }}
+          />
+          <div className="c8_sh py-6">
+            <div className="flex flex-row justify-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="50px"
+                height="50px"
+              >
+                <path
+                  fill="currentColor"
+                  d="M15 2h1c.552 0 1 .448 1 1v2c0 .552-.448 1-1 1h-1c-.552 0-1-.448-1-1V3c0-.552.448-1 1-1zm-1 1v2H6V3h8zM3 6h18c.552 0 1 .448 1 1v14c0 .552-.448 1-1 1H3c-.552 0-1-.448-1-1V7c0-.552.448-1 1-1zm0 16h18V8H3v14zM7 10h10v2H7v-2zm0 4h10v2H7v-2z"
+                ></path>
+              </svg>
+            </div>
+          </div>
+        </a>
+        {/* Ajoutez d'autres liens ici */}
+      </div>
     </div>
   );
 }
