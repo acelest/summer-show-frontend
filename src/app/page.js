54fc3b4752b20d42fc12441e59788a466fd37c9d
Aubin <eyengadjou@gmail.com>
2024-05-08 13:31:44 +0100

    page layouting

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
-// pages/index.js
-
 import Banner from "@/components/Banner";
 import Category from "@/components/CategorySection";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "./globals.css";
 
-// import Layout from "@/components/Layout";
 export default function Home() {
   return (
-    <div className="bg-black ">
+    <div>
       <Header />
       <div>
         <h1 className=" text-blue-50">Welcome to My Next.js App!</h1>
