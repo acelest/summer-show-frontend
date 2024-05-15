6f1f07ec5beb0b977209f3b6333bf79065c60fbe
Aubin <eyengadjou@gmail.com>
2024-05-15 20:48:21 +0100

    category list

---
+++ src/components/CandidatesList.jsx
@@ -0,0 +1,2 @@
+// Créez un composant appelé CandidatesList.js dans le répertoire components.
+// Dans ce composant, recevez les candidats en tant que prop et mappez-les pour les afficher sous forme de liste ou de grille.
