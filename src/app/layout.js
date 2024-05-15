1ab3e90ae3a51605d07a064671f1d0e0474c295d
Aubin <eyengadjou@gmail.com>
2024-05-15 15:04:51 +0100

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,37 +5,16 @@ const inter = Inter({ subsets: ["greek"] });
 
 export const metadata = {
   title: "The Summer Show",
-  description: "A Last Dance",
+  description: "A Last Dance, code by Aubin Djou",
 };
 
 const RootLayout = ({ children }) => (
-  <html lang="fr">
+  <html lang="en">
     <head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
-      <link
-        rel="icon"
-        href="/icon?<generated>"
-        type="image/<generated>"
-        sizes="<generated>"
-      />
-      <link
-        rel="apple-touch-icon"
-        href="/apple-icon?<generated>"
-        type="image/<generated>"
-        sizes="<generated>"
-      />
       <link
         href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap"
         rel="stylesheet"
       />
-      
-      <link
-        rel="icon"
-        href="/icon?<generated>"
-        type="image/png"
-        sizes="32x32"
-      />
-      
     </head>
     <body className={inter.className}>{children}</body>
   </html>
